8042ceb441302b743f3ec038d2e532ed3f4187b3
Chikorita <61445279+chikorita-commits@users.noreply.github.com>
2025-11-11 16:35:55 -0800

    Tidying the code up
    Separated package installation/importation with main analysis code.
    
    Added loading. 
    
    Cleaned up data frame.

--- main.R
+++ main.R
@@ -1,2 +1,9 @@
+#install and import required packages
+install.packages("tidyverse")
 library("tidyverse")
-housing |> filter(!is.na(bedrooms),!is.na(bathrooms))
+#stop installing/importing required packages
+
+#load the csv file
+housing <- read.csv("housing.csv",header=TRUE)
+#clean the data frame
+housing |> filter(!is.na(bedrooms),!is.na(bathrooms)) |> select(-1)
